38fe5445f057b22e68a3ea8bc4a01cdce7b6a513
Gerry Gao <hologerry@gmail.com>
2021-01-05 23:23:29 +0800

    add head

--- detectron2/modeling/roi_heads/roi_heads.py
+++ detectron2/modeling/roi_heads/roi_heads.py
@@ -873,3 +873,17 @@ class StandardROIHeads(ROIHeads):
         else:
             features = dict([(f, features[f]) for f in self.keypoint_in_features])
         return self.keypoint_head(features, instances)
+
+
+@ROI_HEADS_REGISTRY.register()
+class Res5ROIHeadsExtraNorm(Res5ROIHeads):
+    """
+    As described in the MOCO paper, there is an extra BN layer
+    following the res5 stage.
+    """
+    def _build_res5_block(self, cfg):
+        seq, out_channels = super()._build_res5_block(cfg)
+        norm = cfg.MODEL.RESNETS.NORM
+        norm = get_norm(norm, out_channels)
+        seq.add_module("norm", norm)
+        return seq, out_channels
