8d2ac8e977b335fb7876724c03494513b0622d3f
Gerry Gao <hologerry@gmail.com>
2021-05-21 16:49:27 +0800

    fix a bug

--- detectron2/data/__init__.py
+++ detectron2/data/__init__.py
@@ -5,6 +5,7 @@ from .build import (
     build_batch_data_loader,
     build_detection_test_loader,
     build_detection_train_loader,
+    build_detection_train_loader_mini,
     get_detection_dataset_dicts,
     load_proposals_into_dataset,
     print_instances_class_histogram,
